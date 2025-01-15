7fe4a9dac0ea8a8e74af2f85d39e01ba7e35504a
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-15 15:15:24 -0800

    Update.

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
+function selectDifferentDrivers(drivers, a){
+    return a(drivers);
+}
+
 fareDoubler(fare)
 
 fareTripler(fare)
