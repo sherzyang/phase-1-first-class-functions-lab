80c8d6e434b75128edb7ed8bad5cb71a860e3a65
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-15 13:55:05 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (names) {
+    return names.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(names) {
+    return names.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let integer;
+
+function createFareMultiplier(integer){
+    return function(fare){integer * fare;}
+}
+
+const fareDoubler = createFareMultiplier(2)
